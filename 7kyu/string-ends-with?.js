b38e761e137297d550deb5ae813da467a97d3d81
Perry Cole <perrycole1996@gmail.com>
2025-01-28 18:34:32 +0000

    completed String ends with? 7kyu Codewars challenge, also ranked up!

---
+++ 7kyu/string-ends-with?.js
@@ -0,0 +1,33 @@
+// Complete the solution so that it returns true if the first argument(string) passed in ends with the 2nd argument (also a string).
+
+// Examples:
+
+// solution('abc', 'bc') // returns true
+// solution('abc', 'd') // returns false
+
+// My Solution
+
+function solution(str, ending){
+
+    const theEnd = str.slice(-ending.length)
+   
+    if(theEnd === ending || ending === ''){
+      return true
+    } 
+      else {
+        return false
+    }
+}
+
+
+// Best Practice Solution 1
+
+function solution(str, ending){
+    return str.endsWith(ending);
+  }
+
+// Best Practice Solution 2
+
+function solution(str, ending){
+    return str.substr(-ending.length) == ending;
+  }
